e62e935aefe15856c5f86d8fb683d5589065a2c5
Roger102090 <130093686+Roger102090@users.noreply.github.com>
2023-05-07 02:56:44 +0000

    Calculadora terminada

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -3,23 +3,10 @@ using namespace std;
 int main() {
     int n1, n2, n3, op;
     
-        printf("Ingresa dos números y elige la opción deseada entre las siguientes:\n1: Suma\n2: Resta\n3: Multiplicación\n4: División\nIngrese el primer número\n");
-        scanf("%d", &n1);
-        while (n1 > 0)
-        {
-        cuot << "Debes Ingresar un número";
+        printf("Ingresa dos números y elige la opción deseada entre las siguientes:\n1: Suma\n2: Resta\n3: Multiplicación\n4: División\nIngrese el primer número:\n");
         cin >> n1;
-        system("pause");
-        }
-        
         printf("Ingrese el segundo número:\n");
         scanf("%d", &n2);
-        while (n2 > 0)
-        {
-        printf("Debes Ingresar un número");
-        scanf("%d", &n2);
-        }
-        
         printf("Ingrese la opción deseada:\n");
         scanf("%d", &op);
 
