92b78b38b6ee002b7c184f2011d4de45a9c53793
Rogelio Gonzalez <130093686+Roger102090@users.noreply.github.com>
2023-05-08 03:45:13 +0000

    Ejercicio de lectura de variables terminado

---
+++ Lector.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+using namespace std;
+int main()
+{
+    int v1;
+    char const v2[] = "Texto";
+    printf("En el actual programa se van a leer diferentes formatos, definiendo la forma de leer\n");
+    v1 = 10;
+    printf("Numeros %d\n", v1);
+    printf("Caracteres %s\n", v2);
+  
+   
+}
