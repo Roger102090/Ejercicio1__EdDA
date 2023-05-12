ceb32c4345c262846b1abbbe9b5eec7b0fc1bbc4
Rogelio Gonzalez <130093686+Roger102090@users.noreply.github.com>
2023-05-12 21:22:23 +0000

    Calculadora lista

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int n1, n2, n3, opS, opR, opM, opD;
+   float n1, n2, opD, opS, opR, opM;
     
     printf("Ingresa dos números se va a realizar una\nSuma\nResta\nMultiplicación\nDivisión\nIngrese el primer número:\n");
-        cin >> n1;
+       scanf("%f", &n1);
         printf("Ingrese el segundo número:\n");
-        scanf("%d", &n2);
+        scanf("%f", &n2);
             opS = n1 + n2;
             opR = n1 - n2;
             opM = n1 * n2;
             opD = n1 / n2;
-    printf("La respuesta de la operacion suma es: %d\nDe la resta: %d\nDe la multiplicación: %d\nDe la división: %d", opS, opR, opM, opD);        
+    printf("La respuesta de la operacion de\nsuma: %f\nDe la resta: %f\nDe la multiplicación: %f\nDe la división: %f", opS, opR, opM, opD);        
 }
