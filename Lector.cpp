37798ec4b70bb379212b5e38f6ddefa58bf453c8
Rogelio Gonzalez <130093686+Roger102090@users.noreply.github.com>
2023-05-12 02:25:00 +0000

    Avance de lector

--- Lector.cpp
+++ Lector.cpp
@@ -3,12 +3,31 @@
 using namespace std;
 int main()
 {
-    int v1;
-    char const v2[] = "Texto";
-    printf("En el actual programa se van a leer diferentes formatos, definiendo la forma de leer\n");
-    v1 = 10;
-    printf("Numeros %d\n", v1);
-    printf("Caracteres %s\n", v2);
-  
-   
+    // Declaración de variables
+    string nombre[20], eSalud[25], eleccion[0];
+    int peso;
+    float altura;
+    bool elecciones;
+    int formula = peso/(altura*altura);
+    // Ejecución del programa
+    printf("Para calcular tu IMC, debes escribir los siguientes datos\nEscribe tu nombre:\n");
+    scanf("%s", nombre);
+    printf("Escribe tu peso:\n");
+    scanf("%d", &peso);
+    printf("Escribe tu estatura:\n");
+    scanf("%f", &altura);
+    printf("¿Gozas de Buena salud?\n(s) para sí y (n) para no\n");
+    scanf("%s", eleccion);
+        if (eleccion[0] == "s")
+        {
+            elecciones = true;
+            eSalud[21] = "estado de salud bueno";
+        }else{
+            if (eleccion[0] == ("n"))
+            {
+                elecciones = false;
+                eSalud[20] = "estado de salud malo";
+            }
+        };
+    printf("Tu nombre es %s\nTu peso es: %d\nTu altura es: %f\nTienes un IMC de: %s\nRecuerda que tu iMC solo es un referente para que tengas una idea de que peso debes tener para que así puedas cuidar de tu salud de la mejor manera posible", nombre, peso, altura, eSalud, formula);
 }
